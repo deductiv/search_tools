49a8df9c959a9bf54b4c7a49746ce68d7062eed3
J.R. Murray <jr.murray@deductiv.net>
2024-05-20 15:07:02 -0400

    Updates

--- bin/search_restpost.py
+++ bin/search_restpost.py
@@ -19,6 +19,54 @@ class RESTPostCommand(GeneratingCommand):
 	endpoint = Option(require=True)
 
 	def generate(self):
+		args = [val for val in self._metadata.searchinfo.args[2:] if '=' not in val]
+
+		logger.debug("Arguments: " + str(self._metadata.searchinfo.args[2:]))
+		arg_count = len(args)
+		arg_index = 0
+
+		# Parse the arguments to the command
+		if arg_count >= 3:
+			while arg_index < arg_count:
+				# Process the lookup name, lookup field, search field
+				if self.lookup == '':
+					self.lookup = args[arg_index]
+					arg_index += 1
+				if self.lookupfield == '':
+					self.lookupfield = args[arg_index]
+					if len(args) >= arg_index + 2:
+						if args[arg_index + 1].upper() == 'AS':
+							self.searchfield = args[arg_index + 2]
+							arg_index += 3
+						else:
+							self.searchfield = self.lookupfield
+							arg_index += 1
+					else:
+						self.searchfield = self.lookupfield
+						arg_index += 1
+						
+				if arg_index < len(args) and None not in [self.lookup, self.lookupfield, self.searchfield]:
+					if args[arg_index].upper() == 'OUTPUT':
+						self.output_overwrite = True
+					elif args[arg_index].upper() == 'OUTPUTNEW':
+						self.output_overwrite = False
+					else:
+						# Add field to output fields list
+						output_field_name = args[arg_index].strip(',')
+						if len(args) >= arg_index + 2:
+							if args[arg_index + 1].upper() == 'AS':
+								self.output_aliases[output_field_name] = args[arg_index + 2]
+								arg_index += 2
+							else:
+								self.output_aliases[output_field_name] = output_field_name
+						else:
+							self.output_aliases[output_field_name] = output_field_name
+					arg_index += 1
+		else: 
+			logger.critical("Not enough parameters specified to execute.")
+			print("Not enough parameters specified to execute.")
+			exit(1957)
+		
 		output = {
 			'_time': time.time()
 			}
